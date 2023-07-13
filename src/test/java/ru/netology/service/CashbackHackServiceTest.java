36c0885f12e728c8cd7e77301052a0ea0bc8c4a3
123 <nabgulnara@mail.ru>
2023-07-13 20:51:46 +0300

    junit4Tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void shouldReturn0IfAmountIs1000(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+}
